27d36cc2252357a45e364a63a85829202073acd6
Christopher Anoruo <quest4chris2004@gmail.com>
2018-08-23 15:12:15 -0700

    logo component corresponding styles

--- src/components/logo.js
+++ src/components/logo.js
@@ -7,7 +7,7 @@ export default class Logo extends Component {
 
     render() {
         return (
-            <div className>
+            <div className ="logo-main">
                 <img src="/assets/ds_circle_logo.png" alt="daily smarty ui image log big"/>
             </div>
         );
