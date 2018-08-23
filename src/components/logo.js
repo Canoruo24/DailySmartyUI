de3de5c19c61eae71dab724a3d0347d94ea7eb2f
Christopher Anoruo <quest4chris2004@gmail.com>
2018-08-23 15:05:50 -0700

    First commit

---
+++ src/components/logo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className>
+                <img src="/assets/ds_circle_logo.png" alt="daily smarty ui image log big"/>
+            </div>
+        );
+    }
+}
